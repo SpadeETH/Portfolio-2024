8e339fc9518132aab96e44f7749314ba1e0d7a85
Arthur Bossuyt <116507270+SpadeETH@users.noreply.github.com>
2024-08-14 18:06:25 +0200

    feur

--- app/components/ProjectHolder.tsx
+++ app/components/ProjectHolder.tsx
@@ -2,7 +2,7 @@
 import { motion } from "framer-motion";
 import Image from "next/image";
 import Link from "next/link";
-import { useContext } from "react";
+import { useContext, useState } from "react";
 import { FiArrowRight } from "react-icons/fi";
 import { useInView } from "react-intersection-observer";
 import translations from "../../lib/translations";
@@ -38,12 +38,12 @@ const ProjectHolder: React.FC = () => {
   );
 };
 
-// Update ProjectCard to accept buttonText as a prop
 const ProjectCard: React.FC<{ project: Project; buttonText: string }> = ({
   project,
   buttonText,
 }) => {
   const { ref, inView } = useInView({ triggerOnce: true });
+  const [isHovered, setIsHovered] = useState(false);
 
   return (
     <motion.div
@@ -56,6 +56,8 @@ const ProjectCard: React.FC<{ project: Project; buttonText: string }> = ({
         filter: inView ? "blur(0px)" : "blur(8px)",
       }}
       transition={{ duration: 0.6, delay: 0.3 }}
+      onMouseEnter={() => setIsHovered(true)}
+      onMouseLeave={() => setIsHovered(false)}
     >
       <Link href={project.link}>
         <div className={styles.imageContainer}>
@@ -68,8 +70,7 @@ const ProjectCard: React.FC<{ project: Project; buttonText: string }> = ({
           />
           <motion.div
             className={styles.overlay}
-            whileHover={{ opacity: 1 }}
-            initial={{ opacity: 0 }}
+            animate={{ opacity: isHovered ? 1 : 0 }}
             transition={{ duration: 0.3 }}
           >
             <div className={styles.overlayTextWrapper}>
